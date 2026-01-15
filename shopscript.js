1434e8494d8030438e217d05c52b5dfbef10a246
P10NE3R <78094115+P10NE3R@users.noreply.github.com>
2026-01-15 20:57:36 +0100

    3

--- shopscript.js
+++ shopscript.js
@@ -1,19 +1,22 @@
-document.getElementById("cart-button").addEventListener("click",function(){
-   document.getElementById("cart").classList.toggle("hidden");
-});
+document.getElementById("cart-button").addEventListener("click", function() {
+    document.getElementById("cart").classList.toggle("hidden")
+})
 
-//Funksjon for produkt opplisting:
+//funksjon for produktopplisting:
 function fetchProducts() {
     let productHTML = ""
+
     products.map(p => productHTML += `<article class="product-card">
-                <img src="Website_images/PROD_${p.imagefile}" alt ="${p.title}">
+                <img src="website_images/PROD_${p.imagefile}" alt="${p.title}" />
                 <a href="#">${p.category}</a>
                 <h3>${p.title}</h3>
-                <p>Kr.${p.price},-</p>
-                <button onClick="addToCart(${p.prodid})">Legg til handleogn</button>
-
+                <p>Kr. ${p.price},-</p>
+                <button onClick="addToCart(${p.prodid})">Legg til handlevogn</button>
             </article>`)
-    document.getElementById("product-list").innerHTML = productHTML}
+
+    document.getElementById("product-list").innerHTML = productHTML
+}
+
 fetchProducts()
 
 //Legg til i handlevogn
