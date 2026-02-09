f87d32376fc98f4a9e1995fdd3ade49c219d0d71
P10NE3R <78094115+P10NE3R@users.noreply.github.com>
2026-02-09 12:43:08 +0100

    9

--- LEGOdudes_REACT/src/App.jsx
+++ LEGOdudes_REACT/src/App.jsx
@@ -1,4 +1,10 @@
 import './style/lego.css'
+import Header from './components/header.jsx'
+import Nav from "./components/Nav.jsx"
+import Cart from './components/Cart.jsx'
+import ProductCard from './components/ProductCard.jsx'
+import Products from './components/Products.jsx'
+import CartItem from './components/CartItem.jsx'
 import { products } from './assets/legodudes'
 import { useEffect, useState } from 'react'
 function App() {
@@ -6,6 +12,8 @@ function App() {
   const [isOpen, setIsOpen] = useState(false)
   const [cart, setCart] = useState([])
   const [cartQuantity, setCartQuantity] = useState(0)
+  const [totalSum, setTotalSum] = useState(0)
+
 
   console.log("Cart",cart)
 
@@ -15,105 +23,31 @@ useEffect(()=>{
   //reduce() summerer alle tall i en array, nice for å summere en array meld verdier. 
     const totalQuantity = cart.reduce((sum,item) => sum + item.quantity, 0)
     setCartQuantity(totalQuantity)
+    
+    //Her legger vi sammen pris
+    //Dette gjøre ved å legge sammen vare pris og antall i tilegg til sum 
+    const total = cart.reduce(
+      (sum, item) => sum + (item.price * item.quantity),
+       0)
+    setTotalSum(total)
+
   },[cart])
 
   
 
-  function Header({setIsOpen, cartQuantity}){
-    return(
-      <header>
-        <h1>
-          <a href="index.html">             
-            <img src="website_images/LD_logo.svg" alt="LEGOdudes" />
-          </a>
-          </h1>
-          <button id="cart-button" onClick={()=> setIsOpen((prev) => !prev)}>
-            <div id="cart-quantity">{cartQuantity}</div>
-            <img src="website_images/legocart.svg" alt="Handlevogn" />
-          </button>
-      </header>
-    )
-  }
+  
 
-  function Nav(){
-    return(
-       <nav>
-          <a href="#">City</a>
-          <a href="#">Ninjago</a>
-          <a href="#">Castles & Knights</a>
-          <a href="#">Marine & Pirates</a>
-          <a href="#">Movie characters</a>
-        </nav>
-    )
-  }
 
   function CategoryTitle(){
     return (<h2>Ninjago</h2>)
   }
 
-  function Products({products, setCart}){
-    return (
-    <div id="product-list">
-      {products.map(p => <ProductCard key={p.prodid} p={p} setCart={setCart} />)}
-      
-    </div>)
-  }
 
-  function ProductCard({p, setCart}){
-    
-    const handleClick = ()=>{
-      setCart((prev) => 
-        prev.some(item => item.prodid === p.prodid) ? 
-        prev.map(item => item.prodid === p.prodid ? {...item, quantity: item.quantity + 1}
-        : item ) : [...prev, {...p, quantity: 1}])
-      console.log("Legg i handlekurv")
-    }
 
-    return (
-      <article className="product-card">
-          <img src={`website_images/PROD_${p.imagefile}`} alt={p.title} />
-          <a href="#">${p.category}</a>
-          <h3>{p.title}</h3>
-          <p>Kr. {p.price},-</p>
-          <button onClick={handleClick}>Legg til handlevogn</button>
-      </article>
-    )
-  }
 
-  function Cart({isOpen, cart, setCart}){
-    return (
-      <section id="cart" className={isOpen ? "" : "hidden"}>
-            <table id="cart-items">
-              <tbody>
-                {cart.length <= 0 ? 
-                  (<tr>
-                    <td>Ingen varer i handlevognen enda.</td>
-                  </tr>) : (cart.map(p => <CartItem setCart={setCart} key={p.prodid} p = {p}/>))}
-              </tbody>
-            </table>
-            <p>Total pris: <span id="total-price">0</span>NOK</p>
-        </section>
-    )
-  }
+
   
-  function CartItem({p, setCart}){
-    
-    const removeFromCart = (prodid)=>{
-      setCart(prev => prev.map(item => item.prodid === prodid ? {...item, quantity: item.quantity - 1}: item)
-      .filter(item => item.quantity > 0)
-    
-    )
-    }
-    
-    return (
-      <tr>
-        <td className="title">${p.title}</td>
-        <td className="price">${p.price}</td>
-        <td className="quantity">{p.quantity}</td>
-        <td className="delete"><button onClick={() => removeFromCart(p.prodid)}>X</button></td>
-      </tr>
-    )
-  }  
+
 
 
   return (
@@ -124,7 +58,7 @@ useEffect(()=>{
         <CategoryTitle />
         <Products products={products} setCart={setCart} />
       </main>
-      <Cart isOpen={isOpen} cart={cart} setCart={setCart} />
+      <Cart isOpen={isOpen} cart={cart} setCart={setCart} totalSum={totalSum}/>
     </div>
   )
 }
